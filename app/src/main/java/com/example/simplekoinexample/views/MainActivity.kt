cc3a828d052c31e3639837353f072ff1c58d8636
seunggiHong <tmdrl1026@naver.com>
2021-07-30 22:52:48 +0900

    viewbinding + todo 추가 액티비티

--- app/src/main/java/com/example/simplekoinexample/views/MainActivity.kt
+++ app/src/main/java/com/example/simplekoinexample/views/MainActivity.kt
@@ -1,48 +1,71 @@
-package com.example.simplekoinexample
+package com.example.simplekoinexample.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.simplekoinexample.Constants.TAG
+import com.example.simplekoinexample.KoinPresenter
+import com.example.simplekoinexample.MainViewModel
+import com.example.simplekoinexample.R
+import com.example.simplekoinexample.databinding.ActivityMainBinding
 import com.example.simplekoinexample.recyclerview.TodoAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var todoAdapter: TodoAdapter
+    private lateinit var binding: ActivityMainBinding
     private val namePresenter: KoinPresenter by inject()
-    val mainViewModel: MainViewModel by viewModel()
+    private val mainViewModel: MainViewModel by viewModel()
+    lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "MainActivity - onCreate() called")
 
-        tv_name_view.text = namePresenter.sayMyName()
-
         initView()
-        setObserveView()
+        initAdapter()
+        initViewModel()
+
+        binding.fabNewTodo.setOnClickListener {
+            val intent = Intent(this, AddTodoActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun initView() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.tvNameView.text = namePresenter.sayMyName()
+    }
+
+    private fun initAdapter() {
         todoAdapter = TodoAdapter()
         rc_todo_view.adapter = todoAdapter
+    }
 
+    private fun initViewModel() {
         mainViewModel.todoList.observe(this, Observer {
             it.let {
                 todoAdapter.setItems(it)
             }
         })
-
         mainViewModel.fetchTodo()
 
     }
 
-    private fun setObserveView() {
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "MainActivity - onStart() called") 
+    }
 
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "MainActivity - onRestart() called") 
     }
 
     override fun onResume() {
